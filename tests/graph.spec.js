8a37c7eb54a2c6b4f2094def7ed0c61cfbf0fb8d
IanPFo@gmail.com <IanPFo@gmail.com>
2022-08-21 17:48:10 -0500

    Expand testing coverage for graph link forces

--- tests/graph.spec.js
+++ tests/graph.spec.js
@@ -2,14 +2,34 @@ import { Graph } from '../src/game/graph'
 import { Node } from '../src/node'
 import { config } from '../app.config'
 
+import { NodeIcon } from '../src/ui_objects/node_icon'
+import { LinkPoint } from '../src/link_point';
+jest.mock('../src/link_point')
+
+jest.mock('../src/ui_objects/node_icon', () => {
+  return {
+    NodeIcon: jest.fn().mockImplementation(() => {
+      return {}
+    })
+  }
+});
 
 describe('Graph', () => {
   let graph
   let node_1
   let node_2
+  let mockLinkDetectionImplementation
+  let areNodesLinked
 
   beforeEach(() => {
     graph = new Graph
+    LinkPoint.mockClear()
+
+    mockLinkDetectionImplementation = jest
+      .spyOn(LinkPoint.prototype, 'is_linked_to_node_with_identifier')
+      .mockImplementation((identifier) => {
+        return areNodesLinked;
+      })
   })
 
   describe("calculate_force_between_nodes", () => {
@@ -18,11 +38,14 @@ describe('Graph', () => {
     // of the nodes. Ideally we'd be using vector comparisons, like is the force vector
     // pointing towards the other node, or away from it? But for now, we'll depend on manual comparisons.
     describe("when nodes have no linkages", () => {
+      beforeEach(() => {
+        areNodesLinked = false
+      })
 
       describe("when the nodes are closer than the configured standoff distance", () => {
         beforeEach(() => {
-          node_1 = { position: [0, 0] }
-          node_2 = { position: [0, config.graph.node_spacing.STANDOFF_DISTANCE / 2] }
+          node_1 = new Node([0,0])
+          node_2 = new Node([0, config.graph.node_spacing.STANDOFF_DISTANCE / 2])
         })
 
         it('the net force is repulsive', () => {
@@ -34,8 +57,8 @@ describe('Graph', () => {
 
       describe("when the nodes are exactly at the configured standoff distance", () => {
         beforeEach(() => {
-          node_1 = { position: [0, 0] }
-          node_2 = { position: [0, config.graph.node_spacing.STANDOFF_DISTANCE] }
+          node_1 = new Node([0, 0])
+          node_2 = new Node([0, config.graph.node_spacing.STANDOFF_DISTANCE])
         })
 
         it('the net force is exactly zero', () => {
@@ -47,8 +70,8 @@ describe('Graph', () => {
 
       describe("when the nodes are farther than the standoff distance", () => {
         beforeEach(() => {
-          node_1 = { position: [0, 0] }
-          node_2 = { position: [0, config.graph.node_spacing.STANDOFF_DISTANCE * 2] }
+          node_1 = new Node([0, 0])
+          node_2 = new Node([0, config.graph.node_spacing.STANDOFF_DISTANCE * 2] )
         })
 
         it('the net force is attractive', () => {
@@ -61,22 +84,21 @@ describe('Graph', () => {
 
     describe("when nodes are linked", () => {
       beforeEach(() => {
+        areNodesLinked = true
       })
-      describe("when the nodes are exactly at the configured standoff distance", () => {
 
+      describe("when the nodes are exactly at the configured standoff distance", () => {
           beforeEach(() => {
-            node_1 = { position: [0, 0] }
-            node_2 = { position: [0, config.graph.node_spacing.STANDOFF_DISTANCE] }
+            node_1 = new Node([0, 0])
+            node_2 = new Node([0, config.graph.node_spacing.STANDOFF_DISTANCE])
           })
 
-
-        it('the net force is exactly zero', () => {
+        it('the net force is positive', () => {
           var force_vector = graph.calculate_force_between_nodes(node_1, node_2)
           expect(force_vector[0]).toEqualZero()
-          expect(force_vector[1]).toEqualZero()
+          expect(force_vector[1]).toBeGreaterThan(0)
         })
       })
     })
-
   })
 })
